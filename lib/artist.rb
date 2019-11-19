10e4179e3517ee00040131d5bfe3d1c6ed508a57
Ryan M <rmccauleymusic@gmail.com>
2019-11-19 03:09:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,8 @@ attr_accessor :name, :songs
   end
 
   def add_song(song)
-    @songs << song
-    #song.artist = self
+    self.songs << song
+    song.artist = self
   end
 
   def self.find_or_create_by_name(name)
